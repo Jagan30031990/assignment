aa39680dafe8415db348d1e54abd1a4800da46cf
Praarthana Rajmith <praarthana.app@gmail.com>
2022-11-22 13:22:57 +0530

    index page search valiadtion

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -19,12 +19,13 @@ class LoginController extends Controller
     public function home_page(Request $request)
     {
 
+
         $exported_data = DB::table(function ($query) {
             $query->selectRaw('*')
             ->from('mst_export_india')
             ->orderBy('id','asc')
             ->take(2000);
-        })->paginate(100);
+        })->paginate(2000);
 
         $exported_data_count = DB::table(function ($query) {
             $query->selectRaw('*')
@@ -43,93 +44,97 @@ class LoginController extends Controller
 
         foreach ($datas as $data)
         {
-           $dataModified[] = $data->product_name;
-       }        
+            $dataModified[] = $data->product_name;
+        }        
 
-       return response()->json($dataModified);
+        return response()->json($dataModified);
 
-   }
-   public function filter_data(Request $request)
-   {
-    DB::enableQueryLog();
-    $words = explode(" ", $request->search_data);
-    $query = ExportIndia::query();
-    foreach ($words as $word) {
-        $query->where('product_description', 'LIKE', "%{$word}%");
+    }
+    public function filter_data(Request $request)
+    {
+
+        if(is_numeric($request->search_data))
+        {
+            $words = explode(" ", $request->search_data);
+            $query = ExportIndia::query();
+            foreach ($words as $word) {
+                $query->where('hs_code', 'LIKE', "%{$word}%");
+            }
+
+            $exported_data_count = $query->get();
+            $words = explode(" ", $request->search_data);
+            $query = ExportIndia::query();
+            foreach ($words as $word) {
+                $query->where('hs_code', 'LIKE', "%{$word}%");
+            }
+        }
+        else
+        {
+            $words = explode(" ", $request->search_data);
+            $query = ExportIndia::query();
+            foreach ($words as $word) {
+                $query->where('product_description', 'LIKE', "%{$word}%");
+            }
+
+            $exported_data_count = $query->get();
+            $words = explode(" ", $request->search_data);
+            $query = ExportIndia::query();
+            foreach ($words as $word) {
+                $query->where('product_description', 'LIKE', "%{$word}%");
+            }
+        }
+// DB::enableQueryLog();
+        $exported_data = $query->take(2000)->paginate(1000);
+
+        $count_of_data = ExportIndia::count();
+        return view('index', compact('count_of_data','exported_data','exported_data_count'));
+    }
+    public function refresh_captcha()
+    {
+
+        return response()->json([
+            'captcha' => Captcha::img()
+        ]);
+    }
+    public function chklogin(Request $request)
+    {
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|alphaNum|min:3'
+        ]);
+
+        $user_data = array(
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        );
+
+        if (Auth::attempt($user_data)) {
+            Toastr::success('Successfully Login');
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false]);
+        }
+    }
+    public function morePosts()
+    {
+        $export_india = ExportIndia::paginate(50);
+        return response()->json($export_india);
     }
 
-    $exported_data_count = $query->get();
-     $words = explode(" ", $request->search_data);
-    $query = ExportIndia::query();
-    foreach ($words as $word) {
-        $query->where('product_description', 'LIKE', "%{$word}%");
+    public function export_india()
+    {
+        $export_india = ExportIndia::paginate(100);
+        return view('index', compact('export_india'));
     }
 
-    $exported_data = $query->take(2000)->paginate(1000);
-    // print_r(DB::getQueryLog());
-    // die();
-    // $exported_data_count = DB::table(function ($query) {
-    //     $query->selectRaw('*')
-    //     ->from('mst_export_india')
-    //     ->orderBy('id','asc')
-    //     ->take(2000);
-    // });
-    // DB::enableQueryLog();
-    // $exported_data = ExportIndia::selectRaw('*')
-    // ->where('product_description', 'like', '%' . $search_data . '%')
-    // ->from('mst_export_india')
-    // ->orderBy('id','asc')
-    // ->take(2000)->paginate(100);
-
-    $count_of_data = ExportIndia::count();
-    return view('index', compact('count_of_data','exported_data','exported_data_count'));
-}
-public function refresh_captcha()
-{
-
-    return response()->json([
-        'captcha' => Captcha::img()
-    ]);
-}
-public function chklogin(Request $request)
-{
-
-    $this->validate($request, [
-        'email' => 'required|email',
-        'password' => 'required|alphaNum|min:3'
-    ]);
-
-    $user_data = array(
-        'email' => $request->get('email'),
-        'password' => $request->get('password')
-    );
-
-    if (Auth::attempt($user_data)) {
-        Toastr::success('Successfully Login');
-        return response()->json(['success' => true]);
-    } else {
-        return response()->json(['success' => false]);
+    function logout()
+    {
+        Auth::logout();
+        return redirect('/');
     }
-}
-public function morePosts()
-{
-    $export_india = ExportIndia::paginate(50);
-    return response()->json($export_india);
-}
-
-public function export_india()
-{
-    $export_india = ExportIndia::paginate(100);
-    return view('index', compact('export_india'));
-}
-
-function logout()
-{
-    Auth::logout();
-    return redirect('/');
-}
-public function dashboard(Request $request)
-{
-    return view('dashboard');
-}
+    public function dashboard(Request $request)
+    {
+        return view('dashboard');
+    }
 }
